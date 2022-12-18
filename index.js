74ae4e9a53efd7e35f6cf9402de6b75994e8f93c
Ashley Mojica <ashjmojica@gmail.com>
2022-12-18 16:26:02 -0600

    commit tree 9b895340ad63e06d2b5e22724681043002979de9

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+    const employee = {
+        name: "Chelsea",
+        streetAddress: {
+          line1: "11 Broadway",
+          line2: "2nd Floor", 
+      },
+
+    };
+
+    function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+        const copyEmployee = {...employeeObj}
+        console.log("copyEmployee", copyEmployee)
+        copyEmployee[key] = value
+        return copyEmployee
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+        obj[key] = value
+        return obj
+    }
+
+    function deleteFromEmployeeByKey(employeeObj, key, value) {
+        const copyEmployee = {...employeeObj}
+        delete copyEmployee[key]
+        return copyEmployee
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employeeObj, key, value) {
+        delete employeeObj[key]
+        return employeeObj
+    }
+    
